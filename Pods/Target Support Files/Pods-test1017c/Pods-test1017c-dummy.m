cecf03f31a1997faef4f077b8d61d8ef3c882666
dlqud10 <dlqud10@naver.com>
2018-10-17 19:38:48 +0900

    first commit loading page

---
+++ Pods/Target Support Files/Pods-test1017c/Pods-test1017c-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test1017c : NSObject
+@end
+@implementation PodsDummy_Pods_test1017c
+@end
--- test1017c/ViewController.swift
+++ test1017c/ViewController.swift
@@ -7,13 +7,57 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NVActivityIndicatorViewable {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        self.view.backgroundColor = UIColor(red: CGFloat(237 / 255.0), green: CGFloat(85 / 255.0), blue: CGFloat(101 / 255.0), alpha: 1)
+        
+
+     
+        let cellWidth = Int(self.view.frame.width/2)
+        let cellHeight = Int(self.view.frame.height/2)
+
+        let x = cellWidth
+        let y = cellHeight
+        
+        
+        let frame = CGRect(x: x, y: y, width: cellWidth / 2, height: cellHeight / 4)
+
+        
+        print(Int(self.view.frame.width), self.view.frame.height)
+        print(cellWidth, cellHeight)
+        
+            let activityIndicatorView = NVActivityIndicatorView(frame: frame,type: NVActivityIndicatorType.pacman)
+        
+            activityIndicatorView.padding = 10
+        
+            self.view.addSubview(activityIndicatorView)
+ 
+            activityIndicatorView.startAnimating()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+            
+            // Put your code which should be executed with a delay here
+            
+            activityIndicatorView.stopAnimating()
+            
+            let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+            
+            
+            self.present(secondViewController, animated: true, completion: nil)
+            
+            
+            
+        })
+            
+        
+        }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
