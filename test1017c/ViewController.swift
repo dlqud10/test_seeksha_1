9bd31e6f2aa3a78611abe6b1d3caf1e9fc4c9464
moblie <moblie@moblieui-imac.local>
2018-10-17 17:38:32 +0900

    Initial Commit

---
+++ test1017c/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test1017c
+//
+//  Created by moblie on 2018. 10. 17..
+//  Copyright © 2018년 moblie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
